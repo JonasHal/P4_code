134fb7dec319c49485f74ab1ae1c965020e68578
moleseaau <56633631+moleseaau@users.noreply.github.com>
2021-03-30 14:02:45 +0200

    test med JSON

---
+++ testJSON.py
@@ -0,0 +1,12 @@
+import json
+import pandas as pd
+
+testlist = []
+
+with open("universities_latest_all.ndjson", encoding="utf-8") as f:
+    for line in f:
+        testlist.append(line)
+
+#https://stackoverflow.com/questions/40588852/pandas-read-nested-json
+df = pd.read_json('universities_latest_all.ndjson', lines=True)
+print(df.head())
