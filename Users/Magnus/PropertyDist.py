931b8c823d11afd8b18a90c11bfbb8743ba11f38
moleseaau <56633631+moleseaau@users.noreply.github.com>
2021-04-07 20:20:37 +0200

    extractProperties til funktion
    extractProperties er nu en funktion man kan importere.

---
+++ Users/Magnus/PropertyDist.py
@@ -0,0 +1,30 @@
+from extractPropertiesFromNDJSON import extractProperties
+from pathlib import Path
+import pandas as pd
+import plotly.graph_objects as go
+
+property_list = extractProperties(Path("../../Data/universities_latest_all.ndjson"))
+
+
+number_of_properties = len(property_list)
+print(number_of_properties)
+
+property_count = {}
+for lists in property_list:
+    for prop in lists:
+        property_count[prop] = property_count.get(prop, 0) + 1
+
+print(property_count)
+
+property_dataframe = pd.DataFrame(list(property_count.items()), columns=['Property', 'Frequency'])
+property_dataframe = property_dataframe.sort_values(by=['Frequency'], ascending=False)
+print(property_dataframe)
+
+fig = go.Figure()
+fig.add_trace(go.Histogram(x=property_dataframe['Property'], y=property_dataframe['Frequency']))
+fig.show()
+
+
+
+
+
--- extractPropertiesFromNDJSON.py
+++ extractPropertiesFromNDJSON.py
@@ -1,20 +1,30 @@
 import ndjson
 import pandas as pd
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, fpmax, fpgrowth
+from pathlib import Path
 
-filename = "universities_latest_all.ndjson"
-property_list = []
+def extractProperties(filename):
+    """
 
-#Open and read the file to a value
-with open(filename, encoding="utf-8") as f:
-    data = ndjson.load(f)
+    :param filename: What file to open: for example Path("Data/universities_latest_all.ndjson")
+    :return: returns a Python list with all the properties from the .ndjson file.
+    """
 
-df = pd.DataFrame(data)
+    property_list= []
 
-#Walks .ndjson file and extracts the properties
-for i in range(len(df)):
-    property_list.append(list(df["claims"][i].keys()))
+    with open(filename, encoding="utf-8") as f:
+        wikidata = ndjson.load(f)
+
+    wikidata_df = pd.DataFrame(wikidata)
+
+    #Walks the .ndjson file and extracts the properties
+    for i in range(len(wikidata_df)):
+        property_list.append(list(wikidata_df["claims"][i].keys()))
+
+    return property_list
+
+if __name__ == '__main__':
+    property_list = extractProperties(Path("Data/universities_latest_all.ndjson"))
+    print(property_list)
 
 #With apriori algorithm and stuff
 """
