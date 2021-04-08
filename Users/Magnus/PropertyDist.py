2b9c5ec652bac2a048f8e466f0a49c28c50796dc
moleseaau <56633631+moleseaau@users.noreply.github.com>
2021-04-08 21:29:30 +0200

    lavet en funktion der tæller antal properties for hvert uni

--- Users/Magnus/PropertyDist.py
+++ Users/Magnus/PropertyDist.py
@@ -3,27 +3,74 @@ from pathlib import Path
 import pandas as pd
 import plotly.graph_objects as go
 
-property_list = extractProperties(Path("../../Data/universities_latest_all.ndjson"))
 
-number_of_properties = len(property_list)
-print(number_of_properties)
+def property_count_function(listOfProperties):
+    """
 
-property_count = {}
-for lists in property_list:
-    for prop in lists:
-        property_count[prop] = property_count.get(prop, 0) + 1
+    :param listOfProperties: Input is the listOfProperties, use the function extractProperties.
+    The for loop expects a nested list.
+    :return: property_dataframe: a dataframe containing the properties extracted from the list and the
+    frequency of which they appear
+    """
 
-print(property_count)
+    property_count = {}  # Empty dict, is gonna look like this: property_count{property : count}
+    for lists in listOfProperties:
+        for properties in lists:
+            property_count[properties] = property_count.get(properties, 0) + 1
 
-property_dataframe = pd.DataFrame(list(property_count.items()), columns=['Property', 'Frequency'])
-property_dataframe = property_dataframe.sort_values(by=['Frequency'], ascending=False)
-print(property_dataframe)
+    # Converts the dictionary to a dataframe
+    property_dataframe = pd.DataFrame(list(property_count.items()), columns=['Property', 'Frequency'])
+    property_dataframe = property_dataframe.set_index("Property")
+    property_dataframe = property_dataframe.sort_values(by=['Frequency'], ascending=False)
 
-fig = go.Figure()
-fig.add_trace(go.Bar(x=property_dataframe['Property'], y=property_dataframe['Frequency']))
-fig.show()
+    return property_dataframe
 
+def entity_property_count_function(listOfProperties):
+    entity_property_count = {}
+    for lists in listOfProperties:
+        if len(lists) not in entity_property_count:
+            entity_property_count.setdefault(len(lists), 0)
+            entity_property_count.setdefault(0, len(lists))
+        if len(lists) in entity_property_count:
+            entity_property_count[len(lists)] += 1
 
+    entity_property_dataframe = pd.DataFrame(list(entity_property_count.items()), columns=['Number of Properties',
+                                                                                           '#Universities'])
+    entity_property_dataframe = entity_property_dataframe.sort_values(by=['#Universities'], ascending=False)
+    column_titles = ['#Universities', "Number of Properties"]
+    entity_property_dataframe = entity_property_dataframe.reindex(columns=column_titles)
 
+    return entity_property_dataframe
 
+prop_list = extractProperties(Path("../../Data/universities_latest_all.ndjson"))
+df2 = entity_property_count_function(prop_list)
+print(df2)
 
+if __name__ == '__main__':
+
+    property_list = extractProperties(Path("../../Data/universities_latest_all.ndjson"))
+
+    df = property_count_function(property_list)
+    df = df.drop("P31")
+
+    number_of_properties_above_1000 = []
+    number_of_properties_below_1000 = []
+    for index, row in df.iterrows():
+        if row['Frequency'] > 1000:
+            number_of_properties_above_1000.append(index)
+        else:
+            number_of_properties_below_1000.append(index)
+
+    #print(len(number_of_properties_above_1000))
+    #print(len(number_of_properties_below_1000))
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=df.index, y=df['Frequency']))
+    fig.update_layout(
+        xaxis_title="Property",
+        yaxis_title="Property Frequency"
+    )
+    #fig.show()
+
+    #print("The median value for the property frequency is {}".format(df['Frequency'].median()))
+    #print("The average value for the property frequency is {}".format(df['Frequency'].mean()))
