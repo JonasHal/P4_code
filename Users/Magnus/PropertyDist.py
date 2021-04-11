b7abfb9051bf67059c4531cf46311a2baf4cbed9
moleseaau <56633631+moleseaau@users.noreply.github.com>
2021-04-11 14:51:34 +0200

    opdateret kode og figs

--- Users/Magnus/PropertyDist.py
+++ Users/Magnus/PropertyDist.py
@@ -15,8 +15,11 @@ def property_count_function(listOfProperties):
 
     property_count = {}  # Empty dict, is gonna look like this: property_count{property : count}
     for lists in listOfProperties:
-        for properties in lists:
-            property_count[properties] = property_count.get(properties, 0) + 1
+        try:
+            for properties in lists:
+                property_count[properties] = property_count.get(properties, 0) + 1
+        except TypeError as e:
+            print(e)
 
     # Converts the dictionary to a dataframe
     property_dataframe = pd.DataFrame(list(property_count.items()), columns=['Property', 'Frequency'])
@@ -25,33 +28,39 @@ def property_count_function(listOfProperties):
 
     return property_dataframe
 
+
 def entity_property_count_function(listOfProperties):
+    """
+
+    :param listOfProperties: Input is the listOfProperties, use the function extractProperties.
+    The for loop expects a nested list.
+    :return: returns the entity_property_dataframe containing ['Number of Properties'] and how many universites
+    have this exact number in ['#Universities']
+    """
     entity_property_count = {}
     for lists in listOfProperties:
         if len(lists) not in entity_property_count:
             entity_property_count.setdefault(len(lists), 0)
-            entity_property_count.setdefault(0, len(lists))
+            #entity_property_count.setdefault(0, len(lists)) #Er ret sikker på den her linje skal væk
         if len(lists) in entity_property_count:
             entity_property_count[len(lists)] += 1
 
-    entity_property_dataframe = pd.DataFrame(list(entity_property_count.items()), columns=['Number of Properties',
+    entity_property_dataframe = pd.DataFrame(list(entity_property_count.items()), columns=['#Properties',
                                                                                            '#Universities'])
-    entity_property_dataframe = entity_property_dataframe.sort_values(by=['#Universities'], ascending=False)
-    column_titles = ['#Universities', "Number of Properties"]
+    entity_property_dataframe = entity_property_dataframe.sort_values(by=['#Properties'], ascending=True)
+    #The next two lines switches the two columns.
+    column_titles = ['#Universities', "#Properties"]
     entity_property_dataframe = entity_property_dataframe.reindex(columns=column_titles)
 
     return entity_property_dataframe
 
-prop_list = extractProperties(Path("../../Data/universities_latest_all.ndjson"))
-df2 = entity_property_count_function(prop_list)
-print(df2)
 
 if __name__ == '__main__':
 
     property_list = extractProperties(Path("../../Data/universities_latest_all.ndjson"))
 
     df = property_count_function(property_list)
-    df = df.drop("P31")
+    #df = df.drop("P31")
 
     number_of_properties_above_1000 = []
     number_of_properties_below_1000 = []
@@ -61,8 +70,8 @@ if __name__ == '__main__':
         else:
             number_of_properties_below_1000.append(index)
 
-    #print(len(number_of_properties_above_1000))
-    #print(len(number_of_properties_below_1000))
+    # print(len(number_of_properties_above_1000))
+    # print(len(number_of_properties_below_1000))
 
     fig = go.Figure()
     fig.add_trace(go.Bar(x=df.index, y=df['Frequency']))
@@ -70,7 +79,27 @@ if __name__ == '__main__':
         xaxis_title="Property",
         yaxis_title="Property Frequency"
     )
-    #fig.show()
+    fig.show()
 
-    #print("The median value for the property frequency is {}".format(df['Frequency'].median()))
-    #print("The average value for the property frequency is {}".format(df['Frequency'].mean()))
+    # print("The median value for the property frequency is {}".format(df['Frequency'].median()))
+    # print("The average value for the property frequency is {}".format(df['Frequency'].mean()))
+
+    df2 = entity_property_count_function(property_list)
+
+
+    violin_plot = go.Figure(data=go.Violin(y=df2["#Properties"], fillcolor="lightblue", points="all", box_visible=True,
+                                    line_color='black', opacity=0.6, x0="Number of Properties"))
+    #violin_plot.update_yaxes(range=[0, 140])
+    #violin_plot.show()
+
+    university_property_fig = go.Figure()
+    university_property_fig.add_trace(go.Bar(x=df2['#Properties'], y=df2['#Universities']))
+    university_property_fig.update_layout(
+        xaxis_title="Number of Properties",
+        yaxis_title="Number of Universities"
+    )
+    #university_property_fig.show()
+
+    # print("The median value for the property frequency is {}".format(df2['#Properties'].median()))
+    # print("The average value for the property frequency is {}".format(df2['#Properties'].mean()))
+    # print(df2['#Universities'].sum())
