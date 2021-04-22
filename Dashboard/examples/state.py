6d4da00ae7c23eb0de44052df512db8bfdfc01ae
JonasHal <60646377+JonasHal@users.noreply.github.com>
2021-04-22 11:31:51 +0200

    state update

---
+++ Dashboard/examples/state.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+all_options = {
+    'America': ['New York City', 'San Francisco', 'Cincinnati'],
+    'Canada': [u'Montréal', 'Toronto', 'Ottawa']
+}
+app.layout = html.Div([
+    dcc.RadioItems(
+        id='countries-radio',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='America'
+    ),
+
+    html.Hr(),
+
+    dcc.RadioItems(id='cities-radio'),
+
+    html.Hr(),
+
+    html.Div(id='display-selected-values'),
+
+    #State
+    html.H1("State (Independent)"),
+
+    html.Div(
+        [
+            dcc.Input(id="input-1", type="text", value="Montréal"),
+            dcc.Input(id="input-2", type="text", value="Canada"),
+            html.Div(id="number-output"),
+        ]
+    ),
+
+    #Numbers
+    html.H1("Calculations with multiple outputs"),
+
+    html.Div([
+        dcc.Input(
+            id='num-multi',
+            type='number',
+            value=5
+        ),
+
+        html.Table([
+            html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
+            html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
+            html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
+            html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
+            html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
+        ]),
+    ])
+])
+
+
+@app.callback(
+    Output('cities-radio', 'options'),
+    Input('countries-radio', 'value'))
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    Output('cities-radio', 'value'),
+    Input('cities-radio', 'options'))
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    Output('display-selected-values', 'children'),
+    Input('countries-radio', 'value'),
+    Input('cities-radio', 'value'))
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
+
+#State
+@app.callback(
+    Output("number-output", "children"),
+    Input("input-1", "value"),
+    Input("input-2", "value"),
+)
+def update_output(input1, input2):
+    return u'Input 1 is "{}" and Input 2 is "{}"'.format(input1, input2)
+
+#Number
+@app.callback(
+    Output('square', 'children'),
+    Output('cube', 'children'),
+    Output('twos', 'children'),
+    Output('threes', 'children'),
+    Output('x^x', 'children'),
+    Input('num-multi', 'value'))
+def callback_a(x):
+    return x**2, x**3, 2**x, 3**x, x**x
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
