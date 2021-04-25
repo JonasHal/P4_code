34446efb0fab8e6ca7c23556cee841fd23b0bda7
RonniCarlsen <ronni_C@live.dk>
2021-04-25 19:56:33 +0200

    Update Ronni_test.py

--- Users/Ronni/Ronni_test.py
+++ Users/Ronni/Ronni_test.py
@@ -33,7 +33,7 @@ df = pd.DataFrame(te_ary, columns=te.columns_)
 
 # FPgrowth algorithm:
 
-frequent_itemsets_growth = fpgrowth(df, min_support=0.5, use_colnames=True)
+frequent_itemsets_growth = fpgrowth(df, min_support=0.2, use_colnames=True)
 # print(frequent_itemsets_growth.info())
 # print(frequent_itemsets_growth.describe())
 frequent_itemsets_growth["itemsets_len"] = frequent_itemsets_growth["itemsets"].apply(lambda x: len(x))
