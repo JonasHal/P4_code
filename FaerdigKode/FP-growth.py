4faa171789aa2687adc9e746951bec26787a68f3
moleseaau <56633631+moleseaau@users.noreply.github.com>
2021-04-26 10:17:27 +0200

    nu dropper vi ikk P31 i FP-growth

--- FaerdigKode/FP-growth.py
+++ FaerdigKode/FP-growth.py
@@ -9,7 +9,7 @@ property_list = extractProperties(Path("../Data/universities_latest_all.ndjson")
 te = TransactionEncoder()
 te_ary = te.fit(property_list).transform(property_list)
 property_dataframe = pd.DataFrame(te_ary, columns=te.columns_)
-property_dataframe = property_dataframe.drop('P31', axis=1)
+#property_dataframe = property_dataframe.drop('P31', axis=1)
 
 property_label_dataframe = pd.read_csv(Path("../Data/properties.csv"))
 
@@ -21,9 +21,10 @@ for prop in property_dataframe.columns:
         # # This line replaces the P-code with the P label value
         property_dataframe.rename({prop: prop_label_value}, axis='columns', inplace=True)
 
-frequent_properties = fpgrowth(property_dataframe, min_support=0.1, use_colnames=True)
+frequent_properties = fpgrowth(property_dataframe, min_support=0.2, use_colnames=True)
 print(frequent_properties)
-#print(frequent_properties.sort_values(by=['support'], ascending=False).head(5))
+print(len(frequent_properties))
+print(frequent_properties.sort_values(by=['support'], ascending=False).head(5))
 
 #property_rules = association_rules(frequent_properties, metric="lift", min_threshold=3.2)
 #print(property_rules.to_string())
