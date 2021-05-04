dd6779eb1e7ff0c49a0e9ec07a260e24c3a31e90
moleseaau <56633631+moleseaau@users.noreply.github.com>
2021-05-04 10:47:23 +0200

    update til BDS

--- Not_Report/Spark/PySpark_presentation.py
+++ Not_Report/Spark/PySpark_presentation.py
@@ -7,21 +7,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import count, isnull, when, col
 from pyspark.mllib.stat import Statistics
 from pyspark.ml.feature import Imputer
+import seaborn as sns
 
 spark = SparkSession.builder.appName('BDS').getOrCreate()
-#df = spark.read.csv("C:/Users/magnu/Downloads/pima-indians-diabetes.csv")
 df1 = spark.read.options(header="True", inferSchema=True, delimiter=",") \
     .csv("C:/Users/magnu/Downloads/pima-indians-diabetes.csv")
-#df1.printSchema()
+df1.printSchema()
+
 num_cols = ['Number of times pregnant', 'Body mass index', 'Plasma glucose concentration',
             'Diastolic blood pressure', 'Triceps skinfold thickness', '2-Hour serum insulin',
             'Body mass index', 'Diabetes pedigree function', 'Age', 'Class variable']
 
-df1.select(num_cols).describe().show()
+#df1.select(num_cols).describe().show()
 
 test = df1.groupBy('Number of times pregnant').count()
 sorted_test = test.sort(col('Number of times pregnant').asc())
-sorted_test.show(truncate=False)
+#sorted_test.show(truncate=False)
 
 x = sorted_test.select(col('Number of times pregnant')).toPandas()
 y = sorted_test.select(col('count')).toPandas()
@@ -29,26 +30,26 @@ bins = np.arange(0, 17, 1)
 plt.hist(x, bins, histtype='bar', color='blue', weights=y)
 plt.xlabel('Pregnancies')
 plt.ylabel('Count')
-plt.show()
-# plt.bar(x=x, height=y)
-# plt.show()
+#plt.show()
 
 col_names = df1.columns
-features = df1.rdd.map(lambda row: row[0:])
+features = df1.rdd.map(lambda row: row[0:]) #PythonRDD[41] at RDD at PythonRDD.scale:54
 corr_mat = Statistics.corr(features, method="pearson")
+
 corr_df = pd.DataFrame(corr_mat)
 corr_df.index, corr_df.columns = col_names, col_names
 
-print(corr_df.to_string())
+#print(corr_df.to_string())
 
-df1.select([count(when(isnull(c), c)).alias(c) for c in df1.columns]).show()
+#df1.select([count(when(isnull(c), c)).alias(c) for c in df1.columns]).show()
 
 imputer = Imputer(inputCols=["Plasma glucose concentration", "Diastolic blood pressure",
                              "Triceps skinfold thickness", "2-Hour serum insulin", "Body mass index"],
                   outputCols=["Plasma glucose concentration", "Diastolic blood pressure",
                              "Triceps skinfold thickness", "2-Hour serum insulin", "Body mass index"])
-new_df = imputer.fit(df1)
-new_df = new_df.transform(df1)
+
+imputer = imputer.setStrategy('median').setMissingValue(np.nan)
+new_df = imputer.fit(df1).transform(df1)
 
 new_col_names = new_df.columns
 features2 = new_df.rdd.map(lambda row: row[0:])
@@ -60,3 +61,14 @@ corr_df2.index, corr_df2.columns = new_col_names, new_col_names
 
 #imputer.fit(df1).transform(df1).show()
 #imputer.setStrategy('median').setMissingValue(np.nan).fit(df1).transform(df1).show()
+
+# ['Number of times pregnant', 'Body mass index', 'Plasma glucose concentration',
+#             'Diastolic blood pressure', 'Triceps skinfold thickness', '2-Hour serum insulin',
+#             'Diabetes pedigree function', 'Age', 'Class variable']
+
+sns.set(style='ticks')
+sns.pairplot(new_df.toPandas(),
+             x_vars=["Body mass index", "Number of times pregnant", "Age"],
+             y_vars=["Body mass index", "Number of times pregnant"],
+             hue='Class variable', markers=["D", "o"])
+#plt.show()
