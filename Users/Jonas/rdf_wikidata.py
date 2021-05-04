e433121321429f51f748dd11365e8c30946bedb2
JonasHal <60646377+JonasHal@users.noreply.github.com>
2021-05-04 18:10:19 +0200

    Add Retrieve Properties API

--- Users/Jonas/rdf_wikidata.py
+++ Users/Jonas/rdf_wikidata.py
@@ -10,6 +10,7 @@ import requests
 app = dash.Dash(__name__)
 
 SEARCHPAGE = ""
+SEARCHENTITY = "Q314"
 
 colors = {
     'background': '#111111',
@@ -18,36 +19,63 @@ colors = {
 
 
 app.layout = html.Div([
-    html.H1(children='Hello Dash'),
+    html.Header(html.H1(children='Hello Dash')
+                ),
 
     html.Div(children='Dash: A web application framework for Python.', style={
         'textAlign': 'center',
         'color': colors['text']
     }),
 
+    html.H2(children="Search Bar"),
+
     html.Div([
         dcc.Input(id="input-1", type="text", value=SEARCHPAGE),
-        html.Div(id="number-output"),
+        html.Div(id="search-output"),
     ]),
+
+    html.H2(children="Retrieve Properties"),
+
+    html.Div([
+        dcc.Input(id="input-2", type="text", value=SEARCHENTITY, debounce=True),
+        html.Div(id="properties-output")
+    ])
 
 ])
 
 @app.callback(
-    Output("number-output", "children"),
+    Output("search-output", "children"),
     Input("input-1", "value"),
 )
 def update_output(input1):
     S = requests.Session()
-    URL = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=%s&format=json&limit=10&formatversion=2&language=en" % (input1)
+    URL = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=%s&format=json&limit=5&formatversion=2&language=en" % (input1)
 
     if len(input1) >= 1:
         R = S.post(url=URL, headers={"user-agent" : "magic browser"})
         DATA = R.json()
+        print(DATA)
 
-        return print(DATA)
+        return "sd"
     else:
         return ""
 
+#Bruger mediawiki API wbgetclaims til at hente claims fra en item
+@app.callback(
+    Output("properties-output", "children"),
+    Input("input-2", "value"),
+)
+def extract_properties(input2):
+    S = requests.Session()
+    URL = "https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=%s&format=json" % (input2)
+
+    R = S.post(url=URL, headers={"user-agent": "magic browser"})
+    DATA = R.json()
+    DATA_df = pd.DataFrame(DATA)
+
+    property_list = list(DATA_df.index[1:])
+
+    return property_list
 
 if __name__ == '__main__':
     app.run_server(debug=True)
